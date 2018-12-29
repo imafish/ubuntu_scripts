498ad97fd58ac21003ee8fcd1449ba748b390e38
imafish.Guo <imafish.i+git@gmail.com>
2018-12-29 12:40:41 +0800

    updated avatar.sh to check if $PS1 exists before printing avatar

--- data/avatar.sh
+++ data/avatar.sh
@@ -1,10 +1,11 @@
 
 # show avatar
-if [ `tput colors` -lt 9 ]; then
-  echo 'TODO placeholder -- ASCII art for 8 color console'
-else
-  cat ~/.block.txt
-  echo
-  echo
+if [ "$PS1" ]; then
+  if [ `tput colors` -lt 9 ]; then
+    echo 'TODO placeholder -- ASCII art for 8 color console'
+  else
+    cat ~/.block.txt
+    echo
+    echo
+  fi
 fi
-
