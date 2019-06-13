6e8bf5dd991e1254db8253d84f7aa42eb7ce0f27
Guo Xiaobin <imafish.i@gmail.com>
2019-06-13 21:06:24 +0800

    adds a script to configure variety

---
+++ scripts/desktop_210_configure_variety.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+########################################################
+# set up terminal command
+# works for Gnome Terminal 3.6.2
+
+source_file=../data/variety.conf
+target_file=~/.config/variety/variety.conf
+
+cp $source_file $target_file
+
