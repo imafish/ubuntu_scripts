10329ca9008bda45fb90fc5c3df0b7671a31d058
Guo Xiaobin <imafish.i@gmail.com>
2019-06-13 22:44:20 +0800

    ask apt to fix dependencies after installing chrome

--- scripts/custom_install_chrome.sh
+++ scripts/custom_install_chrome.sh
@@ -3,5 +3,6 @@
 installer="/tmp/chrome_installer_`date +%s`.deb"
 wget -O ${installer} https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i ${installer}
+apt install --fix-broken -y
 rm ${installer}
 
