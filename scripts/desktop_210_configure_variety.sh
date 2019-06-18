3ab1c469301992c9952ba154632232e84ccc3946
Guo Xiaobin <imafish.i@gmail.com>
2019-06-18 13:12:38 +0800

    update scripts for variety

--- scripts/desktop_210_configure_variety.sh
+++ scripts/desktop_210_configure_variety.sh
@@ -5,7 +5,13 @@
 # works for Gnome Terminal 3.6.2
 
 source_file=../data/variety.conf
-target_file=~/.config/variety/variety.conf
+target_dir=~/.config/variety
+target_file=$target_dir/variety.conf
+
+if [ ! -d $target_dir ]; then
+  mkdir -p $target_dir
+  chown -R $USER:$USER ~/.config
+fi
 
 cp $source_file $target_file
 chown $USER:$USER $target_file
